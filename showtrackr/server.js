8145dfa5f5c94dcfd921dfc312d97e32284dcc32
wzaghal <wisamzaghal@gmail.com>
2014-06-24 15:02:33 -0400

    add api routes to get shows

--- showtrackr/server.js
+++ showtrackr/server.js
@@ -68,12 +68,50 @@ var bodyParser = require('body-parser');
 
 
 var app = express();
+
+// app.get('/api/shows', function(req, res, next) {
+//   var query = Show.find();
+
+//   //if genre param passed
+//   if (req.query.genre) {
+//     query.where({ genre: req.query.genre });
+//   }
+//   //if alphabet parameter passed
+//   else if (req.query.alphabet) {
+//     query.where({ name: new RegExp('^' + '[' + req.query.alphabet + ']', 'i') });
+//   }
+//   else {
+//     query.limit(12);
+//   }
+//   //execute query
+//   query.exec(function(err, shows) {
+//     if (err) return next(err);
+//     res.send(shows);
+//   });
+// });
+
+
+// app.get('/api/shows/:id', function(req, res, next) {
+//   Show.findById(req.params.id, function(err, show) {
+//     if (err) return next(err);
+//     res.send(show);
+//   });
+// });
+
+// //Error middleware - returns error
+// app.use(function(err, req, res, next) {
+//   console.error(err.stack);
+//   res.send(500, { message: err.message });
+// });
+
+
 app.set('port',process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
