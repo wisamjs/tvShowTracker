86dde419c44076d9c7e0a4c735d5adf4b83b0120
wzaghal <wisamzaghal@gmail.com>
2014-06-24 17:16:37 -0400

    return error if incorrect route

--- showtrackr/server.js
+++ showtrackr/server.js
@@ -69,40 +69,50 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-// app.get('/api/shows', function(req, res, next) {
-//   var query = Show.find();
+app.get('/api/shows', function(req, res, next) {
+  var query = Show.find();
 
-//   //if genre param passed
-//   if (req.query.genre) {
-//     query.where({ genre: req.query.genre });
-//   }
-//   //if alphabet parameter passed
-//   else if (req.query.alphabet) {
-//     query.where({ name: new RegExp('^' + '[' + req.query.alphabet + ']', 'i') });
-//   }
-//   else {
-//     query.limit(12);
-//   }
-//   //execute query
-//   query.exec(function(err, shows) {
-//     if (err) return next(err);
-//     res.send(shows);
-//   });
-// });
+  //if genre param passed
+  if (req.query.genre) {
+    query.where({ genre: req.query.genre });
+  }
+  //if alphabet parameter passed
+  else if (req.query.alphabet) {
+    query.where({ name: new RegExp('^' + '[' + req.query.alphabet + ']', 'i') });
+  }
+  else {
+    query.limit(12);
+  }
+  //execute query
+  query.exec(function(err, shows) {
+    if (err) return next(err);
+    res.send(shows);
+  });
+});
 
 
-// app.get('/api/shows/:id', function(req, res, next) {
-//   Show.findById(req.params.id, function(err, show) {
-//     if (err) return next(err);
-//     res.send(show);
-//   });
-// });
+app.get('/api/shows/:id', function(req, res, next) {
+  Show.findById(req.params.id, function(err, show) {
+    if (err) return next(err);
+    res.send(show);
+  });
+});
 
-// //Error middleware - returns error
-// app.use(function(err, req, res, next) {
-//   console.error(err.stack);
-//   res.send(500, { message: err.message });
-// });
+/*
+Below code will allow users to refresh page
+and not get weird "CANNOT GET /Add" error
+common problem with HTML5 pushState on the client-side.
+*/
+/*Currently causing redirect loop errors */
+app.get('*', function(req, res) {
+  res.redirect('/#' + req.originalUrl);
+});
+
+//Error middleware - returns error
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.send(500, { message: err.message });
+});
 
 
 app.set('port',process.env.PORT || 3000);
